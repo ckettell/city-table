9329d8ad82bead2b539b15656dc323f5f8802e2c
Charles Kettell <charleskettell@hotmail.co.uk>
2019-10-08 16:54:05 +0100

    can filter by city

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 
+const makeDefaultState = () => ({
+  sorted: [],
+  page: 0,
+  pageSize: 10,
+  expanded: {},
+  resized: [],
+  filtered: []
+});
+
 class Table extends Component {
   constructor(){
     super();
-    this.state = {
-      columns: '',
-      data: ''
-    }
+      this.state = makeDefaultState();
+      // this.resetState = this.resetState.bind(this);
+
   }
 
   componentDidMount = () => {
@@ -42,6 +50,26 @@ class Table extends Component {
     <div>
     <ReactTable
     columns={this.setColumns()}
+    data={this.props.data}
+    pivotBy={["City"]}
+    filterable
+    defaultPageSize={10}
+    className="-striped -highlight"
+    // Controlled props
+    sorted={this.state.sorted}
+    page={this.state.page}
+    pageSize={this.state.pageSize}
+    expanded={this.state.expanded}
+    resized={this.state.resized}
+    filtered={this.state.filtered}
+    // Callbacks
+    onSortedChange={sorted => this.setState({ sorted })}
+    onPageChange={page => this.setState({ page })}
+    onPageSizeChange={(pageSize, page) =>
+      this.setState({ page, pageSize })}
+    onExpandedChange={expanded => this.setState({ expanded })}
+    onResizedChange={resized => this.setState({ resized })}
+    onFilteredChange={filtered => this.setState({ filtered })}
 
     />
 
@@ -49,5 +77,7 @@ class Table extends Component {
   )
 }
 }
+
+
 
 export default Table;
